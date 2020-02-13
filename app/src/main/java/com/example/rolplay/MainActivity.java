f03a41d56cd2f2d189051b746065b98f3e8edd56
Pekezuna <raul.cerro.isern@gmail.com>
2020-02-13 15:35:38 +0100

    commit 1: prova boton

--- app/src/main/java/com/example/rolplay/MainActivity.java
+++ app/src/main/java/com/example/rolplay/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.rolplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mbuton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
