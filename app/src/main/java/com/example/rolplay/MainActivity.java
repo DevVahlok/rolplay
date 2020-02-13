5bf6bed34c3ef8d72b24fcd56264c5e18a5398a5
DevVahlok <alextornerorobles@gmail.com>
2020-02-13 15:31:49 +0100

    Commit inicial: projecte.

---
+++ app/src/main/java/com/example/rolplay/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rolplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
