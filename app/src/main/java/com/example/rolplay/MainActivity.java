0f80130f0edc666704172819a7c6d005f2cb937b
DevVahlok <alextornerorobles@gmail.com>
2020-02-18 16:11:01 +0100

    Fuente en título y material aplicado al login

--- app/src/main/java/com/example/rolplay/MainActivity.java
+++ app/src/main/java/com/example/rolplay/MainActivity.java
@@ -6,13 +6,9 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mbuton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mbuton.setText("tonto");
-        //merge done
     }
 }
