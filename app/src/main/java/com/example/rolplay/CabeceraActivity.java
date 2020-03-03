31cf64f8e6530c4266ca7ef13550190e467a9842
DevVahlok <alextornerorobles@gmail.com>
2020-03-03 20:29:23 +0100

    Cabecera Activity modificada.

--- app/src/main/java/com/example/rolplay/CabeceraActivity.java
+++ app/src/main/java/com/example/rolplay/CabeceraActivity.java
@@ -2,16 +2,25 @@ package com.example.rolplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class CabeceraActivity extends AppCompatActivity {
 
     //Declaración de variables
     private EditText mEditTextNombrePersonaje;
     private Spinner mDropdownRaza;
+    private ProgressBar mBarraProgreso;
+    private int mNivel, mProgresoExperiencia, mExperienciaTotal;
+    private TextView mExperiencia, mNivel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +35,17 @@ public class CabeceraActivity extends AppCompatActivity {
         String[] listaRazas = new String[]{"Bardo","Brujo","Bárbaro","Clérigo","Druida","Explorador","Guerrero","Hechicero","Mago","Paladín","Pícaro"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_oscuro, listaRazas);
-
         mDropdownRaza.setAdapter(adapter);
+        mNivel = findViewById(R.id.CabeceraActivity_tituloNivel);
+        mExperiencia = findViewById(R.id.CabeceraActivity_tituloExperiencia);
+        mBarraProgreso = findViewById(R.id.CabeceraActivity_nivel_barraProgreso);
+        mBarraProgreso.setMax(mExperienciaTotal);
+        mBarraProgreso.setProgress(mProgresoExperiencia);
+        mExperiencia.setText(mProgresoExperiencia+" / "+mExperienciaTotal);
+        mNivel.setText("Nivel "+mNivel);
+
+
+
 
     }
 }
