562b4bf7b0e84d2efb7037b171ddc995f39ed911
Pekezuna <raul.cerro.isern@gmail.com>
2020-03-05 17:44:34 +0100

    Commit intento de fetch a la mierda de firebase que no sale pq esto es una

--- app/src/main/java/com/example/rolplay/CabeceraActivity.java
+++ app/src/main/java/com/example/rolplay/CabeceraActivity.java
@@ -1,9 +1,12 @@
 package com.example.rolplay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,6 +16,17 @@ import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
 public class CabeceraActivity extends AppCompatActivity {
 
     //Declaración de variables
@@ -21,6 +35,7 @@ public class CabeceraActivity extends AppCompatActivity {
     private ProgressBar mBarraProgreso;
     private int mNivel, mProgresoExperiencia, mExperienciaTotal;
     private TextView mExperiencia_ET, mNivel_ET;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +48,36 @@ public class CabeceraActivity extends AppCompatActivity {
         mNivel_ET = findViewById(R.id.CabeceraActivity_tituloNivel);
         mExperiencia_ET = findViewById(R.id.CabeceraActivity_tituloExperiencia);
         mBarraProgreso = findViewById(R.id.CabeceraActivity_nivel_barraProgreso);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         //TODO: Recoger lista de razas de FireBase
         String[] listaRazas = new String[]{"Bardo","Brujo","Bárbaro","Clérigo","Druida","Explorador","Guerrero","Hechicero","Mago","Paladín","Pícaro"};
 
+        final DatabaseReference mRazas = mDatabase.child("DungeonAndDragons").child("Raza");
+
+        FirebaseHelper db = new FirebaseHelper(mRazas);
+
+        final ArrayList Razas = db.retrieve();
+
+        mDatabase.child("DungeonAndDragons").child("Raza").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()){
+                    Razas.add(ds.getKey());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
         //Setea Array al dropdown de Raza
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_oscuro, listaRazas);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_oscuro, Razas);
         mDropdownRaza.setAdapter(adapter);
 
         //TODO: Recoger Nivel y Experiencia de Firebase
@@ -53,6 +92,100 @@ public class CabeceraActivity extends AppCompatActivity {
         //Actualización de los valores en pantalla
         mExperiencia_ET.setText(getString(R.string.experiencia,Integer.toString(mProgresoExperiencia),Integer.toString(mExperienciaTotal)));
         mNivel_ET.setText(getString(R.string.nivelPersonaje, Integer.toString(mNivel)));
+    }
+}
 
+class FirebaseHelper {
+
+    DatabaseReference db;
+    Boolean saved=null;
+    ArrayList<String> spacecrafts=new ArrayList<>();
+
+    public FirebaseHelper(DatabaseReference db) {
+        this.db = db;
+    }
+
+    //WRITE
+    public Boolean save(Spacecraft spacecraft)
+    {
+        if(spacecraft==null)
+        {
+            saved=false;
+        }else
+        {
+            try
+            {
+                db.child("Spacecraft").push().setValue(spacecraft);
+                saved=true;
+
+            }catch (DatabaseException e)
+            {
+                e.printStackTrace();
+                saved=false;
+            }
+        }
+
+        return saved;
+    }
+
+    //READ
+    public ArrayList<String> retrieve()
+    {
+        db.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                fetchData(dataSnapshot);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                fetchData(dataSnapshot);
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return spacecrafts;
+    }
+
+    private void fetchData(DataSnapshot dataSnapshot)
+    {
+        spacecrafts.clear();
+
+        for (DataSnapshot ds : dataSnapshot.getChildren())
+        {
+            String name=ds.getKey();
+            spacecrafts.add(name);
+        }
+    }
+}
+
+class Spacecraft {
+
+    String name;
+
+    public Spacecraft() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
